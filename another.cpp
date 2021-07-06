c1f45b2b976bb7ffa76cdb0c9ea062ef290ef6a1
lancelot <root@lancelot.net>
2021-07-06 17:32:36 +0800

    I've finished another job.

---
+++ another.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: another.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2021年07月06日 星期二 17时30分56秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "just walk and see. But you must keep on!" << endl;
+    return 0;
+}
